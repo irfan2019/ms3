a2c40aefe6f37cf70d070c6ec0a34cc052d415ac
irfan2019 <48289347+irfan2019@users.noreply.github.com>
2022-06-30 11:34:04 +0530

    success

---
+++ application_query/Controllers/course_tableController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using application_query.Models;
+
+namespace application_query.Controllers
+{
+    public class course_tableController : Controller
+    {
+        private application_databaseEntities db = new application_databaseEntities();
+
+        // GET: course_table
+        public ActionResult Index()
+        {
+            return View(db.course_table.ToList());
+        }
+
+        // GET: course_table/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course_table course_table = db.course_table.Find(id);
+            if (course_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course_table);
+        }
+
+        // GET: course_table/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: course_table/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,age,email_id,profession,marks_12th,ug_degree,address,mobile_no,courses")] course_table course_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.course_table.Add(course_table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course_table);
+        }
+
+        // GET: course_table/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course_table course_table = db.course_table.Find(id);
+            if (course_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course_table);
+        }
+
+        // POST: course_table/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,age,email_id,profession,marks_12th,ug_degree,address,mobile_no,courses")] course_table course_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(course_table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course_table);
+        }
+
+        // GET: course_table/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            course_table course_table = db.course_table.Find(id);
+            if (course_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(course_table);
+        }
+
+        // POST: course_table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            course_table course_table = db.course_table.Find(id);
+            db.course_table.Remove(course_table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ application_query/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("application_query")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("application_query")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5e7b26b-1d00-40a6-833d-f8f552c237ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
